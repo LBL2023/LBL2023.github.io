70306b11d2b05b28d1f7e5d576fa913de38e7205
LBL2023 <125893079+LBL2023@users.noreply.github.com>
2023-02-21 01:56:11 +0700

    Update java.js

--- java.js
+++ java.js
@@ -28,10 +28,6 @@ function updateRankedTeams() {
   }
 }
 
-// Call the sortTeamsByWins and updateRankedTeams functions to calculate and display the ranked positions of the teams
-sortTeamsByWins();
-updateRankedTeams();
-
 // JavaScript code to generate more basketballs 
 function addBalls() {
   // Create 10 new basketball elements
@@ -55,3 +51,45 @@ function addBalls() {
 
 // Call the function to add the basketballs on page load
 addBalls();
+
+// Rank team according to table
+function rankTeams() {
+  // Get the table element
+  var table = document.querySelector("table");
+
+  // Get the rows of the table (excluding the first row, which contains headers)
+  var rows = Array.from(table.rows).slice(1);
+
+  // Sort the rows based on the "Winning Games" and "Points" columns
+  rows.sort(function(a, b) {
+    var aWins = parseInt(a.cells[2].textContent);
+    var bWins = parseInt(b.cells[2].textContent);
+    if (aWins !== bWins) {
+      return bWins - aWins;
+    } else {
+      var aPoints = parseInt(a.cells[4].textContent);
+      var bPoints = parseInt(b.cells[4].textContent);
+      return bPoints - aPoints;
+    }
+  });
+
+  // Update the "Ranking" column based on the sorted order
+  rows.forEach(function(row, index) {
+    row.cells[0].textContent = index + 1;
+  });
+
+  // Re-insert the sorted rows into the table
+  rows.forEach(function(row) {
+    table.tBodies[0].appendChild(row);
+  });
+}
+
+// Call the rankTeams function to initially rank the teams
+rankTeams();
+
+
+// Call the sortTeamsByWins and updateRankedTeams functions to calculate and display the ranked positions of the teams
+sortTeamsByWins();
+updateRankedTeams();
+
+
